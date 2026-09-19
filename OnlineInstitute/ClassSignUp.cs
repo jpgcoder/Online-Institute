fd4cfd4055dad2d1c085915ba37b603deb0d4088
jpgcoder <jpegah06@gmail.com>
2026-09-19 15:02:40 +0330

    Final files

---
+++ OnlineInstitute/ClassSignUp.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnlineInstitute
+{
+    public partial class ClassSignUp : Form
+    {
+        public static string lvl = "";
+        public static string chosenclass = "";
+        public ClassSignUp()
+        {
+            InitializeComponent();
+            namelbl.Text = "";
+            usernamelbl.Text = "";
+            namelbl.Text = publicInfo.publicName;
+            usernamelbl.Text = publicInfo.publicUsername;
+
+            // سوال اول
+            questionOne.Text = "1.";
+            question1answer1.Text = "";
+            question1answer2.Text = "";
+            question1answer3.Text = "";
+            question1answer4.Text = "";
+
+            //سوال دوم
+            questionTwo.Text = "2.";
+            question2answer1.Text = "";
+            question2answer2.Text = "";
+            question2answer3.Text = "";
+            question2answer4.Text = "";
+
+            //سوال سوم
+            questionThree.Text = "3.";
+            question3answer1.Text = "";
+            question3answer2.Text = "";
+            question3answer3.Text = "";
+            question3answer4.Text = "";
+        }
+
+        private void answersbtn_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            if (question1answer2.Checked)
+            {
+                i++;
+            }
+            if (question2answer2.Checked)
+            {
+                i++;
+            }
+            if (question3answer2.Checked)
+            {
+                i++;
+            }
+            switch (i)
+            {
+                case 0:
+                    lvl = "تازه کار";
+                    break;
+                case 1:
+                    lvl = "مبتدی";
+                    break;
+                case 2:
+                    lvl = "متوسط";
+                    break;
+                case 3:
+                    lvl = "حرفه ای";
+                    break;
+            }
+            chosenclass = classlist.SelectedItem.ToString();
+            classes.publicClass = chosenclass;
+            classes.publicLvl = lvl;
+            MessageBox.Show(lvl,"سطح شما",MessageBoxButtons.OK,MessageBoxIcon.Information,MessageBoxDefaultButton.Button1,MessageBoxOptions.RightAlign);
+            this.Close();
+            FinalForm frm= new FinalForm();
+            frm.ShowDialog();
+            
+        }
+
+        private void classlist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            if (classlist.SelectedItem == "زبان انگلیسی")
+            {
+                panel1.RightToLeft = RightToLeft.No;
+                // سوال اول
+                questionOne.Text = "1.What is the plural form of book?";
+                question1answer1.Text = "bookes";
+                question1answer2.Text = "books";
+                question1answer3.Text = "bookies";
+                question1answer4.Text = "book's";
+
+                //سوال دوم
+                questionTwo.Text = "2.Which word is a verb?";
+                question2answer1.Text = "apple";
+                question2answer2.Text = "run";
+                question2answer3.Text = "blue";
+                question2answer4.Text = "happy";
+
+                //سوال سوم
+                questionThree.Text = "3.Choose the correct 'present' sentence";
+                question3answer1.Text = "She go to school";
+                question3answer2.Text = "She goes to school";
+                question3answer3.Text = "She going to school";
+                question3answer4.Text = "She gone to school";
+
+            }
+            else if (classlist.SelectedItem == "زبان آلمانی")
+            {
+                panel1.RightToLeft = RightToLeft.No;
+
+                // سوال اول
+                questionOne.Text = "1.Was bedeutet \"Guten Morgen\" auf Persisch?";
+                question1answer1.Text = "شب به خیر";
+                question1answer2.Text = "صبح به خیر";
+                question1answer3.Text = "سلام";
+                question1answer4.Text = "خداحافظ";
+
+                //سوال دوم
+                questionTwo.Text = "2.Welcher Artikel ist richtig für das Wort \"Tisch\"?";
+                question2answer1.Text = "die";
+                question2answer2.Text = "der";
+                question2answer3.Text = "das";
+                question2answer4.Text = "den";
+
+                //سوال سوم
+                questionThree.Text = "3.Was ist die Zahl \"vier\"";
+                question3answer1.Text = "2";
+                question3answer2.Text = "4";
+                question3answer3.Text = "3";
+                question3answer4.Text = "5";
+            }
+            else if (classlist.SelectedItem == "زبان ترکی")
+            {
+                panel1.RightToLeft = RightToLeft.No;
+
+                // سوال اول
+                questionOne.Text = "1.\"Merhaba\" ne anlama gelir?";
+                question1answer1.Text = "خداحافظ";
+                question1answer2.Text = "سلام";
+                question1answer3.Text = "روز خوش";
+                question1answer4.Text = "شب به خیر";
+
+                //سوال دوم
+                questionTwo.Text = "2.halgisi bir renktir?";
+                question2answer1.Text = "kedi";
+                question2answer2.Text = "mavi";
+                question2answer3.Text = "kalen";
+                question2answer4.Text = "masa";
+
+                //سوال سوم
+                questionThree.Text = "3.\"ben\" zamiri ne anlama geliri";
+                question3answer1.Text = "تو";
+                question3answer2.Text = "من";
+                question3answer3.Text = "او";
+                question3answer4.Text = "ما";
+            }
+            else if (classlist.SelectedItem == "پایتون")
+            {
+                panel1.RightToLeft = RightToLeft.Yes;
+
+                // سوال اول
+                questionOne.Text = "1.خروجی print(2+3*4) چیست؟";
+                question1answer1.Text = "20";
+                question1answer2.Text = "14";
+                question1answer3.Text = "24";
+                question1answer4.Text = "10";
+
+                //سوال دوم
+                questionTwo.Text = "2.برای دریافت ورودی از کاربر در پایتون از کدام تابع استفاده می شود؟";
+                question2answer1.Text = "get()";
+                question2answer2.Text = "input()";
+                question2answer3.Text = "scan()";
+                question2answer4.Text = "read()";
+
+                //سوال سوم
+                questionThree.Text = "3.کدام یک از گزینه های زیر list را در پایتون ایجاد می کند؟";
+                question3answer1.Text = "(3,2,1)";
+                question3answer2.Text = "[3,2,1]";
+                question3answer3.Text = "{3,2,1}";
+                question3answer4.Text = "\"3,2,1\"";
+            }
+            else if (classlist.SelectedItem == "سی شارپ")
+            {
+                panel1.RightToLeft = RightToLeft.Yes;
+
+                // سوال اول
+                questionOne.Text = "1.خروجی Console.WriteLine(2+3*5) چیست؟";
+                question1answer1.Text = "25";
+                question1answer2.Text = "17";
+                question1answer3.Text = "15";
+                question1answer4.Text = "21";
+
+                //سوال دوم
+                questionTwo.Text = "2.کدام گزینه برای تعریف متغیر عدد صحیح است؟";
+                question2answer1.Text = "int x = \"10\"";
+                question2answer2.Text = "int x = 10";
+                question2answer3.Text = "string x = 10";
+                question2answer4.Text = "float x = \"10.0\"";
+
+                //سوال سوم
+                questionThree.Text = "3.برای دریافت ورودی از کاربر از کدام تابع استفاده می شود؟";
+                question3answer1.Text = "Console.ReadKey()";
+                question3answer2.Text = "Console.ReadLine()";
+                question3answer3.Text = "input()";
+                question3answer4.Text = "get()";
+            }
+        }
+    }
+    
+}
---
+++ OnlineInstitute/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace OnlineInstitute
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.signupbtn = new System.Windows.Forms.Button();
+            this.loginbtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.viewclassesbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("B Titr", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(170, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(593, 85);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "به موسسه آنلاین ما خوش آمدید!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("B Nazanin", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(260, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(388, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "برای ورود یا ثبت نام روی دکمه های زیر کلیک کنید.";
+            // 
+            // signupbtn
+            // 
+            this.signupbtn.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.signupbtn.Location = new System.Drawing.Point(260, 304);
+            this.signupbtn.Name = "signupbtn";
+            this.signupbtn.Size = new System.Drawing.Size(76, 37);
+            this.signupbtn.TabIndex = 2;
+            this.signupbtn.Text = "ثبت نام";
+            this.signupbtn.UseVisualStyleBackColor = true;
+            this.signupbtn.Click += new System.EventHandler(this.signupbtn_Click);
+            // 
+            // loginbtn
+            // 
+            this.loginbtn.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.loginbtn.Location = new System.Drawing.Point(567, 304);
+            this.loginbtn.Name = "loginbtn";
+            this.loginbtn.Size = new System.Drawing.Size(75, 37);
+            this.loginbtn.TabIndex = 3;
+            this.loginbtn.Text = "ورود";
+            this.loginbtn.UseVisualStyleBackColor = true;
+            this.loginbtn.Click += new System.EventHandler(this.loginbtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("B Nazanin", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(209, 406);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(501, 33);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "در غیر این صورت برای مشاهده کلاس ها روی دکمه زیر کلیک کنید.";
+            // 
+            // viewclassesbtn
+            // 
+            this.viewclassesbtn.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.viewclassesbtn.Location = new System.Drawing.Point(403, 459);
+            this.viewclassesbtn.Name = "viewclassesbtn";
+            this.viewclassesbtn.Size = new System.Drawing.Size(118, 37);
+            this.viewclassesbtn.TabIndex = 5;
+            this.viewclassesbtn.Text = "مشاهده کلاس ها";
+            this.viewclassesbtn.UseVisualStyleBackColor = true;
+            this.viewclassesbtn.Click += new System.EventHandler(this.viewclassesbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::OnlineInstitute.Properties.Resources.green_background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(906, 577);
+            this.Controls.Add(this.viewclassesbtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.loginbtn);
+            this.Controls.Add(this.signupbtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "خوش آمدید!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button signupbtn;
+        private System.Windows.Forms.Button loginbtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button viewclassesbtn;
+    }
+}
+
